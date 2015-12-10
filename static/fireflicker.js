1573065b60f9193a6da8aa1023e90efe4013b85d
bleepbloopsify <bleepbloopsify@gmail.com>
2015-12-10 18:44:01 -0500

    yay

--- static/fireflicker.js
+++ static/fireflicker.js
@@ -1,3 +1,29 @@
-var click = function() {
-  return bleh;
+$(document).ready(function(){
+  console.log("hi");
+  if(localStorage.fire){
+    $("#fire").val(localStorage.fire);
+    console.log(localStorage.fire);
+  }else{
+    localStorage.fire = 0;
+  }
+  $("#fireclick").click( function(){
+    clickFire();
+  });
+  $("#reset").click(function(){
+    setFire(0);
+  })
+});
+
+var setFire = function(num){
+  localStorage.fire = num;
+  $("#fire").html(num);
+};
+
+var setBellow = function(num){
+  localStorage.bellow = num;
+  $("#bellow").html(num);
+};
+
+var clickFire = function(){
+  setFire(++localStorage.fire);
 };
